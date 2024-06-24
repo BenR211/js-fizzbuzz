2edfc34928da42041395418f2b9b695f65a03c3a
Ben Remmer <Ben.Remmer@softwire.com>
2024-06-24 15:28:09 +0100

    made fizz buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,19 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+
+    for (var i = 0 ; i < 100; i ++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log(i + " fizzbuzz");
+        }
+        else if(i % 3 == 0){
+            console.log(i + " fizz")
+
+        }
+        else if (i % 5 === 0){
+            console.log(i + " buzz");
+        }
+        //console.log("Hello, World!");
+    }
 
     // Put your code here...
 
